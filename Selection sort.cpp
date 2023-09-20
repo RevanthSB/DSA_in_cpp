2d2860fd46c1b774e94bb7ff5363e9f0f777d466
Revanth SB <75414332+RevanthSB@users.noreply.github.com>
2023-09-20 10:04:39 +0530

    Add files via upload

---
+++ Selection sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void selectionsort(int *arr, int size) {
+	vector<int> val;
+	for (int i = 0; i < size - 1; i++) {
+		int min = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+		}
+		if (min != i) {
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+}
+int main() {
+	int *arr = new int[5];
+	cout << "enter the elements" << endl;
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+	cout << endl;
+	selectionsort(arr, 5);
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+}
